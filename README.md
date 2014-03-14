5e5103553854aee439b6c136c5e6e4aee8c5cc9e
Peter Chng <pc6411@gmail.com>
2014-03-13 22:21:13 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+# SoftLayer Java Data Type Generator
+
+## What this does
+
+Generates a Java class based on the webpage of a SoftLayer Data Type reference API. (E.G. https://sldn.softlayer.com/reference/datatypes/SoftLayer_Account)
+
+## Usage
+1. `git clone` this repository.
+2. `pip install -r requirements.txt`
+3. `python generate.py -p <package name> <URL to SoftLayer Data Type API reference>`
+4. See `python generate.py --help` for full usage options.
+
+## Example
+`python generate.py -p my.company.com https://sldn.softlayer.com/reference/datatypes/SoftLayer_Account`
+
+The command above will generate a file named `Account.java` in the current folder with the following contents:
+
+```java
+package my.company.com;
+
+import java.util.Date;
+
+public class Account {
+  private Integer accountStatusId;
+  private String address1;
+  private String address2;
+  private Integer allowedPptpVpnQuantity;
+  private String alternatePhone;
+  private Integer brandId;
+  private String city;
+  private Boolean claimedTaxExemptTxFlag;
+  private String companyName;
+  private String country;
+  private Date createDate;
+  private String email;
+  private String faxPhone;
+  private String firstName;
+  private Integer id;
+  private Integer isReseller;
+  private String lastName;
+  private Boolean lateFeeProtectionFlag;
+  private Date modifyDate;
+  private String officePhone;
+  private String postalCode;
+  private String state;
+  private Date statusDate;
+
+}
+```
+
+## Notes
+* Relational and Count properties are not included.
+* Getters/setters aren't implemented. Your IDE can do this task for you and you can choose how to do it.
+* Wrapper types are used instead of primitives; this makes it easier to discern when a property was missing from an unmarshalling operation, rather than being present but set to its default value.
+* Classes should be ready to be used by [Jackson](https://github.com/FasterXML/jackson) without any annotations (once the getters/setters are defined) as the field names match 1-1 with the property names.
+
+## Background
+
+The [SoftLayer APIs](https://sldn.softlayer.com/reference/overview) are pretty well documented.  Unfortunately, there doesn't appear to be an XSD for their [REST API](http://sldn.softlayer.com/article/rest), meaning you can't auto-generate Java classes using `xjc`. However, this is a problem for me as I'm lazy and don't like copying and pasting a bunch of field names to make a Java class.
+
+### Python to rescue
+
+Thankfully, the [SoftLayer Data Types](https://sldn.softlayer.com/reference/datatypes/) are fairly well-documented and using [BeautifulSoup](http://www.crummy.com/software/BeautifulSoup/) made it easy to extract the property names and their types in order to convert the result into a Java class.
---
+++ generate.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+# Tested in Python 2.7.x
+import requests
+import argparse
+from bs4 import BeautifulSoup
+
+import logging
+
+# Argparse setup.
+parser = argparse.ArgumentParser(
+  description="Generate data classes based on SoftLayer Data Types. This will save the output into a ClassName.java file, overwriting any existing file unless the --output option is used.",
+  add_help=True
+)
+parser.add_argument(
+  'datatype_url',
+  type=str,
+  help="URL of the SoftLayer Data Type in the API documentation",
+)
+parser.add_argument(
+  '-n', '--name',
+  type=str,
+  default=None,
+  help="The name of the class; defaults to name of the data type minus the 'SoftLayer_' prefix with underscores converted to CamelCase.",
+  dest='name',
+)
+parser.add_argument(
+  '-p', '--package',
+  type=str,
+  default=None,
+  help="The package of the class; default is no package.",
+  dest='package',
+)
+parser.add_argument(
+  '-v',
+  type=str,
+  default='private',
+  help="The visibility of the data fields.",
+  dest='visibility',
+)
+parser.add_argument(
+  '--logger',
+  type=str,
+  default='output.log',
+  help="Log file name. Default: output.log",
+  dest='log_file'
+)
+parser.add_argument(
+  '--output',
+  action='store_true',
+  help="Output to console instead of saving to file.",
+  dest='output_to_console'
+)
+args = parser.parse_args()
+
+# Logging configuration.
+log_date_format = "%Y-%m-%d %H:%M:%S %Z"
+logging.basicConfig(
+  format="[%(levelname)s:%(asctime)s] %(message)s", 
+  datefmt=log_date_format,
+  filename=args.log_file
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Mapping of SoftLayer data types to Java data types.
+sl_to_java = {
+  'integer': 'Integer',
+  'string': 'String',
+  'boolean': 'Boolean',
+  'dateTime': 'Date',
+  'decimal': 'BigDecimal',
+}
+# Java types that require an import.
+java_imports = {
+  'Date': 'import java.util.Date;',
+  'BigDecimal': 'import java.math.BigDecimal;',
+}
+
+# It is completely insecure to set verify=false, but some systems may not trust the root CA for the SoftLayer TLS certificate.
+r = requests.get(args.datatype_url, verify=False)
+html = BeautifulSoup(r.text)
+
+class_name = args.name
+if not class_name:
+  # Will encounter IndexError if this doesn't exist.
+  class_name = html.select('h1.title .service')[0].text
+  class_name = class_name.replace('SoftLayer_', '').replace('_', '')
+
+# NOTE: The markup is invalid in that there are two elements with id="properties"; 
+# assume the first is the local properties we are concerned about.
+properties = html.select('#properties')
+if len(properties) < 2:
+  raise Exception("No properties found.")
+
+properties = properties[0]
+
+# Sanity check, not strictly necessary; makes things more fragile.
+title = properties.find(name='h2', class_='pane-title')
+if not title or "Local Properties" not in title.text:
+  raise Exception("Could not find any local properties.")
+
+output_fields = ''
+converted_types = set()
+properties = properties.select('.views-row')
+for prop in properties:
+  # If anything changes, should encounter an IndexError since the result list will be empty.
+  field_name = prop.select('.views-field-title .field-content')[0].text
+  sl_data_type = prop.select('.views-field-field-type-value .field-content')[0].text
+
+  if sl_data_type not in sl_to_java:
+    logger.warn("Excluding {} because the type {} was not recognized.".format(field_name, sl_data_type))
+    continue
+
+  # This isn't really needed since we build the output field list here.
+  java_type = sl_to_java[sl_data_type]
+  converted_types.add(java_type)
+  output_fields += '  {} {} {};\n'.format(args.visibility, java_type, field_name)
+
+# Build the list of imports.
+imports = ''
+for java_type, import_statement in java_imports.iteritems():
+  if java_type in converted_types:
+    imports += import_statement + '\n';
+
+with open('class.template') as template_file:
+  template = template_file.read()
+  package_statement = 'package {};\n\n'.format(args.package) if args.package else ''
+  output = template.format(
+    package=package_statement, 
+    import_statements=imports, 
+    class_name=class_name, 
+    fields=output_fields
+  )
+
+  if args.output_to_console:
+    print output
+  else:
+    with open('{}.java'.format(class_name), 'w') as output_file:
+      output_file.write(output)
